5b5ac1a60a6264dcb9d8a4251aaf432303fcd93b
PatricioIribarneCatella <patricio_14_94@hotmail.com>
2017-10-07 10:15:12 -0300

    Refactor para las routes

--- src/index.js
+++ src/index.js
@@ -4,9 +4,11 @@ const path = require('path');
 var bodyParser = require('body-parser');
 var cors = require('cors'); 
 var log4js = require('log4js');
+
 log4js.configure(path.join(__dirname, './config/log4js.json'));
 
-const app = express();
+// express app
+var app = require('./routes/index');
 
 // set body parser limits and type for application/json
 app.use(bodyParser.json());
@@ -20,36 +22,6 @@ app.set('port', process.env.PORT || 5000);
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, 'client/build')));
 
-var basePath = '/api';
-
-// default endpoint
-var defapi = require('./routes/default');
-app.use(basePath, defapi);
-
-// /users endpoint
-var users = require('./routes/users');
-app.use(basePath + '/users', users);
-
-// /paymethods endpoint
-var paymethods = require('./routes/paymethods');
-app.use(basePath + '/paymethods', paymethods);
-
-// /trips endpoint
-var trips = require('./routes/trips');
-app.use(basePath + '/trips', trips);
-
-// /servers endpoint
-var servers = require('./routes/servers');
-app.use(basePath + '/servers', servers);
-
-// /token endpoint
-var token = require('./routes/token');
-app.use(basePath + '/token', token);
-
-// /business-users endpoint
-var business_users = require('./routes/business_users');
-app.use(basePath + '/business-users', business_users);
-
 // Log folder
 try {
 	require('fs').mkdirSync('./log');
@@ -60,9 +32,9 @@ try {
 	}
 }
 
-app.listen(app.get('port'));
-
 var log = log4js.getLogger("consola");
+
+app.listen(app.get('port'));
 
 if (process.env.NODE_ENV !== 'test') {
 	log.info('App listening on port %s: ', app.get('port'));
