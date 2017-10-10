c3d5fc22e797e3890fdd9a6a0f98400c356524d8
PatricioIribarneCatella <patricio_14_94@hotmail.com>
2017-10-09 22:14:01 -0300

    migratiosn

---
+++ db/migrations/20171009220745_app_users.js
@@ -0,0 +1,26 @@
+
+exports.up = function(knex, Promise) {
+
+	return knex.schema.createTable('app_users', (table) => {
+		table.increments();
+		table.string('_ref').notNullable();
+		table.string('applicationOwner');
+		table.string('type').notNullable();
+		table.specificType('cars', 'json[]');
+		table.string('username').notNullable().unique();
+		table.string('name').notNullable();
+		table.string('surname').notNullable();
+		table.string('country').notNullable();
+		table.string('email').notNullable();
+		table.string('birthdate').notNullable();
+		table.specificType('images', 'text[]');
+		table.specificType('balance', 'json[]');
+		table.string('password').notNullable();
+		table.specificType('fb', 'json').notNullable();
+	});
+};
+
+exports.down = function(knex, Promise) {
+
+	return knex.schema.dropTableIfExists('app_users');
+};
