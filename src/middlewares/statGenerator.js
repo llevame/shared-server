acf573ea99c45d8e7a5ca32c36e4cb3cff93004e
PatricioIribarneCatella <patricio_14_94@hotmail.com>
2017-11-26 21:06:53 -0300

    Arreglo de errores varios al correr los tests

--- src/middlewares/statGenerator.js
+++ src/middlewares/statGenerator.js
@@ -3,26 +3,31 @@
 
 var urlParser = require('url');
 var moment = require('moment');
+var log = require('log4js').getLogger("error");
 var error = require('../handlers/error-handler');
 var statsQ = require('../../db/queries-wrapper/stats_queries');
 
 function generateStat(req, res, next) {
 
-	let stat = {
-		app_id: req.user.id,
-		endpoint: urlParser.parse(req.originalUrl).pathname,
-		method: req.method,
-		madeTime: moment().unix()
-	};
+	if (!process.env.NODE_ENV.includes('test')) {
+		let stat = {
+			app_id: req.user.id,
+			endpoint: urlParser.parse(req.originalUrl).pathname,
+			method: req.method,
+			madeTime: moment().unix()
+		};
 
-	statsQ.add(stat)
-		.then(() => {
-			next();
-		})
-		.catch((error) => {
-			log.error("Error: " + err.message + " on: " + req.originalUrl);
-			return res.status(500).json(error.unexpected(err));
-		});
+		statsQ.add(stat)
+			.then(() => {
+				next();
+			})
+			.catch((err) => {
+				log.error("Error: " + err.message + " on: " + req.originalUrl);
+				return res.status(500).json(error.unexpected(err));
+			});
+	} else {
+		next();
+	}
 }
 
 module.exports = {generateStat};
